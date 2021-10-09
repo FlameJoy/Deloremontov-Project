eeb99bb54ae26d42619e43571ffa70e0ae4c667f
FlameJoy <ilia.val07@gmail.com>
2021-10-09 13:37:49 +0300

    Adding styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,22 @@
 "use strict"
 
+let headerSliderIndex = 0;
+showSlides();
+
+function showSlides(n) {
+  let i,
+  slides = document.getElementsByClassName("header-slide-container");
+
+  for(i = 0; i < slides.length; i ++) {
+    slides[i].style.display = "none";
+  }
+
+  headerSliderIndex++;
+
+  if(headerSliderIndex > slides.length) {
+    headerSliderIndex = 1;
+  }
+
+  slides[headerSliderIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000);
+}
