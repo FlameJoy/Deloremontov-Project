d3c6fa4dc46846012a8a7a74f65d8b946b7b2f8e
FlameJoy <ilia.val07@gmail.com>
2021-10-13 21:22:39 +0300

    Create modal menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,3 +72,13 @@ function handleWindowLoad() {
         poster[0].style.visibility = "visible";
     }
 }
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
