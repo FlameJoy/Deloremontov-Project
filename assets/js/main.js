05dbec8a067c9af1bae6878b9fc17ed15f5e3621
FlameJoy <ilia.val07@gmail.com>
2021-11-10 23:10:38 +0300

    Adding video presentation adaptivity

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,23 +54,23 @@ document.getElementById("defaultOpen").click();
 window.addEventListener("DOMContentLoaded", handleWindowLoad);
 
 function handleWindowLoad() {
-    let video = document.getElementById("video");
-    let btn = document.getElementById("play");
-    let poster = document.getElementsByClassName("presentation-wrap");
-    let control = document.getElementsByClassName("video-control");
+  let video = document.getElementById("video");
+  let btn = document.getElementById("play");
+  let poster = document.getElementsByClassName("presentation-wrap");
+  let control = document.getElementsByClassName("video-control");
 
-    btn.addEventListener("click", playVideo);
-    video.addEventListener("click", pauseVideo);
+  btn.addEventListener("click", playVideo);
+  video.addEventListener("click", pauseVideo);
 
-    function playVideo() {
-        video.play();
-        poster[0].style.visibility = "hidden";
-        control[0].style.transition = "all 0s ease";
-    }
+  function playVideo() {
+    video.play();
+    poster[0].style.visibility = "hidden";
+    control[0].style.transition = "all 0s ease";
+  }
     function pauseVideo() {
         video.pause();
         poster[0].style.visibility = "visible";
-    }
+  }
 }
 
 // Get the modal
