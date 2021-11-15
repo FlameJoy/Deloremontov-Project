4b50efd74af42d79f7880890725d36fd7b3d1af8
FlameJoy <ilia.val07@gmail.com>
2021-11-15 21:22:23 +0300

    Changing portfolio pic, adding instagram link

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,6 +78,7 @@ let recall = document.getElementById('id01'),
     redecorating = document.getElementById('id02'),
     overhaul = document.getElementById('id03'),
     designer = document.getElementById('id04');
+    instagram = document.getElementById('instagram-modal');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -89,81 +90,140 @@ window.onclick = function(event) {
     overhaul.style.display = "none";
   } else if (event.target == designer) {
     designer.style.display = "none";
+  } else if (event.target == instagram) {
+    instagram.style.display = "none";
   }
 }
 
 // ------------------
-// Townhouse function
+// Promotion function
 // ------------------
-function townhouseChangeImg(num) {
+function promotionImg(num) {
   if(num == 1) {
-    document.getElementById('townhouse').style.background = 'url(assets/images/dest/main_002.jpg) no-repeat center';
+    document.getElementById('promotions').style.background = 'url(assets/images/dest/portfolio/promotions/promotions-2.jpg) no-repeat center';
   } if(num == 2) {
-    document.getElementById('townhouse').style.background = 'url(assets/images/dest/main_003.jpg) no-repeat center';
+    document.getElementById('promotions').style.background = 'url(assets/images/dest/portfolio/promotions/promotions-3.jpg) no-repeat center';
   } if(num == 3) {
-    document.getElementById('townhouse').style.background = 'url(assets/images/dest/main_004.jpg) no-repeat center';
+    document.getElementById('promotions').style.background = 'url(assets/images/dest/portfolio/promotions/promotions-4.jpg) no-repeat center';
   } if(num == 4) {
-    document.getElementById('townhouse').style.background = 'url(assets/images/dest/main_005.jpg) no-repeat center';
-  } 
+    document.getElementById('promotions').style.background = 'url(assets/images/dest/portfolio/promotions/promotions-5.jpeg) no-repeat center';
+  } if(num == 5) {
+    document.getElementById('promotions').style.background = 'url(assets/images/dest/portfolio/promotions/promotions-6.jpg) no-repeat center';
+  } if(num == 6) {
+    document.getElementById('promotions').style.background = 'url(assets/images/dest/portfolio/promotions/promotions-7.jpg) no-repeat center';
+  } if(num == 7) {
+    document.getElementById('promotions').style.background = 'url(assets/images/dest/portfolio/promotions/promotions-8.jpg) no-repeat center';
+  } if(num == 8) {
+    document.getElementById('promotions').style.background = 'url(assets/images/dest/portfolio/promotions/promotions-9.jpg) no-repeat center';
+  } if(num == 9) {
+    document.getElementById('promotions').style.background = 'url(assets/images/dest/portfolio/promotions/promotions-10.jpg) no-repeat center';
+  }
 }
 
-function townhouseRechangeImg() {
-  document.getElementById('townhouse').style.background = 'url(assets/images/dest/main_001.jpg) no-repeat center';
+function promotionRechangeImg() {
+  document.getElementById('promotions').style.background = 'url(assets/images/dest/portfolio/promotions/promotions-1.jpg) no-repeat center';
+}
+
+// ------------------
+// Room function
+// ------------------
+function roomChangeImg(num) {
+  if(num == 1) {
+    document.getElementById('room-1').style.background = 'url(assets/images/dest/portfolio/room/room-2.jpeg) no-repeat center';
+  } if(num == 2) {
+    document.getElementById('room-1').style.background = 'url(assets/images/dest/portfolio/room/room-3.jpeg) no-repeat center';
+  } if(num == 3) {
+    document.getElementById('room-1').style.background = 'url(assets/images/dest/portfolio/room/room-4.jpeg) no-repeat center';
+  } if(num == 4) {
+    document.getElementById('room-1').style.background = 'url(assets/images/dest/portfolio/room/room-5.jpeg) no-repeat center';
+  } if(num == 5) {
+    document.getElementById('room-1').style.background = 'url(assets/images/dest/portfolio/room/room-6.jpeg) no-repeat center';
+  } if(num == 6) {
+    document.getElementById('room-1').style.background = 'url(assets/images/dest/portfolio/room/room-7.jpeg) no-repeat center';
+  } if(num == 7) {
+    document.getElementById('room-1').style.background = 'url(assets/images/dest/portfolio/room/room-8.jpeg) no-repeat center';
+  } if(num == 8) {
+    document.getElementById('room-1').style.background = 'url(assets/images/dest/portfolio/room/room-9.jpeg) no-repeat center';
+  } if(num == 9) {
+    document.getElementById('room-1').style.background = 'url(assets/images/dest/portfolio/room/room-10.jpeg) no-repeat center';
+  }
+}
+
+function roomRechangeImg() {
+  document.getElementById('room-1').style.background = 'url(assets/images/dest/portfolio/room/room-1.jpeg) no-repeat center';
 }
 
 // ----------------------
-// Country House function
+// Office function
 // ----------------------
-function countryHouseImg(num) {
+function officeImg(num) {
   if(num == 1) {
-    document.getElementById('country-house').style.background = 'url(assets/images/dest/main_012.jpg) no-repeat center';
+    document.getElementById('office-1').style.background = 'url(assets/images/dest/portfolio/office/office-2.jpeg) no-repeat center';
   } if(num == 2) {
-    document.getElementById('country-house').style.background = 'url(assets/images/dest/main_013.jpg) no-repeat center';
+    document.getElementById('office-1').style.background = 'url(assets/images/dest/portfolio/office/office-3.jpeg) no-repeat center';
   } if(num == 3) {
-    document.getElementById('country-house').style.background = 'url(assets/images/dest/main_014.jpg) no-repeat center';
+    document.getElementById('office-1').style.background = 'url(assets/images/dest/portfolio/office/office-4.jpeg) no-repeat center';
   } if(num == 4) {
-    document.getElementById('country-house').style.background = 'url(assets/images/dest/main_015.jpg) no-repeat center';
+    document.getElementById('office-1').style.background = 'url(assets/images/dest/portfolio/office/office-5.jpeg) no-repeat center';
+  } if(num == 5) {
+    document.getElementById('office-1').style.background = 'url(assets/images/dest/portfolio/office/office-6.jpeg) no-repeat center';
+  } if(num == 6) {
+    document.getElementById('office-1').style.background = 'url(assets/images/dest/portfolio/office/office-7.jpeg) no-repeat center';
+  } if(num == 7) {
+    document.getElementById('office-1').style.background = 'url(assets/images/dest/portfolio/office/office-8.jpeg) no-repeat center';
+  } if(num == 8) {
+    document.getElementById('office-1').style.background = 'url(assets/images/dest/portfolio/office/office-9.jpeg) no-repeat center';
   }
 }
 
-function countryHouseRechangeImg() {
-  document.getElementById('country-house').style.background = 'url(assets/images/dest/main_011.jpg) no-repeat center';
+function officeRechangeImg() {
+  document.getElementById('office-1').style.background = 'url(assets/images/dest/portfolio/office/office-1.jpeg) no-repeat center';
 }
 
 // ---------------
-// Flat-1 function
+// Bathroom function
 // ---------------
-function flat1Img(num) {
+function bathroomImg(num) {
   if(num == 1) {
-    document.getElementById('flat-1').style.background = 'url(assets/images/dest/main_022.jpg) no-repeat center';
+    document.getElementById('bathroom-1').style.background = 'url(assets/images/dest/portfolio/bathroom/bathroom-2.jpeg) no-repeat center';
   } if(num == 2) {
-    document.getElementById('flat-1').style.background = 'url(assets/images/dest/main_023.jpg) no-repeat center';
-  } if(num == 3) {
-    document.getElementById('flat-1').style.background = 'url(assets/images/dest/main_024.jpg) no-repeat center';
+    document.getElementById('bathroom-1').style.background = 'url(assets/images/dest/portfolio/bathroom/bathroom-3.jpeg) no-repeat center';
   } if(num == 4) {
-    document.getElementById('flat-1').style.background = 'url(assets/images/dest/main_025.jpg) no-repeat center';
+    document.getElementById('bathroom-1').style.background = 'url(assets/images/dest/portfolio/bathroom/bathroom-5.jpeg) no-repeat center';
+  } if(num == 5) {
+    document.getElementById('bathroom-1').style.background = 'url(assets/images/dest/portfolio/bathroom/bathroom-6.jpeg) no-repeat center';
+  } if(num == 6) {
+    document.getElementById('bathroom-1').style.background = 'url(assets/images/dest/portfolio/bathroom/bathroom-7.jpeg) no-repeat center';
   }
 }
 
-function flat1RechangeImg() {
-  document.getElementById('flat-1').style.background = 'url(assets/images/dest/main_021.jpg) no-repeat center';
+function bathroomRechangeImg() {
+  document.getElementById('bathroom-1').style.background = 'url(assets/images/dest/portfolio/bathroom/bathroom-1.jpeg) no-repeat center';
 }
 
 // ---------------
-// Flat-2 function
+// Сeiling function
 // ---------------
-function flat2Img(num) {
+function ceilingImg(num) {
   if(num == 1) {
-    document.getElementById('flat-2').style.background = 'url(assets/images/dest/main_032.jpg) no-repeat center';
+    document.getElementById('ceiling-1').style.background = 'url(assets/images/dest/portfolio/ceiling/ceiling-2.jpeg) no-repeat center';
   } if(num == 2) {
-    document.getElementById('flat-2').style.background = 'url(assets/images/dest/main_033.jpg) no-repeat center';
+    document.getElementById('ceiling-1').style.background = 'url(assets/images/dest/portfolio/ceiling/ceiling-3.jpeg) no-repeat center';
   } if(num == 3) {
-    document.getElementById('flat-2').style.background = 'url(assets/images/dest/main_034.jpg) no-repeat center';
+    document.getElementById('ceiling-1').style.background = 'url(assets/images/dest/portfolio/ceiling/ceiling-4.jpeg) no-repeat center';
   } if(num == 4) {
-    document.getElementById('flat-2').style.background = 'url(assets/images/dest/main_035.jpg) no-repeat center';
+    document.getElementById('ceiling-1').style.background = 'url(assets/images/dest/portfolio/ceiling/ceiling-5.jpeg) no-repeat center';
+  } if(num == 5) {
+    document.getElementById('ceiling-1').style.background = 'url(assets/images/dest/portfolio/ceiling/ceiling-6.jpeg) no-repeat center';
+  } if(num == 6) {
+    document.getElementById('ceiling-1').style.background = 'url(assets/images/dest/portfolio/ceiling/ceiling-7.jpeg) no-repeat center';
+  } if(num == 7) {
+    document.getElementById('ceiling-1').style.background = 'url(assets/images/dest/portfolio/ceiling/ceiling-8.jpeg) no-repeat center';
+  } if(num == 8) {
+    document.getElementById('ceiling-1').style.background = 'url(assets/images/dest/portfolio/ceiling/ceiling-9.jpeg) no-repeat center';
   }
 }
 
-function flat2RechangeImg() {
-  document.getElementById('flat-2').style.background = 'url(assets/images/dest/main_031.jpg) no-repeat center';
+function ceilingRechangeImg() {
+  document.getElementById('ceiling-1').style.background = 'url(assets/images/dest/portfolio/ceiling/ceiling-1.jpeg) no-repeat center';
 }
